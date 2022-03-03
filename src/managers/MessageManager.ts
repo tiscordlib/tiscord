94bd17309f7dd11d0a0b11ef4d5e3fb11855fc40
MakufonSkifto <63517174+emirsurmen@users.noreply.github.com>
2022-03-03 22:44:01 +0300

    Implement fetchMessages

--- src/managers/MessageManager.ts
+++ src/managers/MessageManager.ts
@@ -1,5 +1,5 @@
 import { APIMessage } from 'discord-api-types/v10';
-import { Client, Message, TextChannel } from '../';
+import { APIError, Client, Message, TextChannel } from '../';
 
 /**
  * Class managing messages
@@ -29,5 +29,37 @@ export class MessageManager {
         data = new Message(this.client, data);
         this.client.cache.messages.set(this.channel.id, data);
         return data;
+    }
+
+    /**
+     * Fetches messages from the current channel
+     * @param {string} around - Get messages around this message ID
+     * @param {string} before - Get messages before this message ID
+     * @param {string} after - Get messages after this message ID
+     * @param {number} limit - Max number of messages to return (1-100)
+     * @returns {Promise<Array<Message>>}
+     */
+    async fetchMessages(around?: string, before?: string, after?: string, limit?: number): Promise<Array<Message>> {
+        const params = new URLSearchParams();
+        params.append('around', around);
+        params.append('before', before);
+        params.append('after', after);
+        params.append('limit', limit.toString());
+
+        const request = (await this.client.rest.get(`/channels/${this.channel.id}/messages`, {
+            query: params
+        })) as any;
+
+        if (request?.code) {
+            throw new APIError(request?.message);
+        }
+
+        const map = request.map(m => new Message(this.client, m));
+
+        map.forEach(m => {
+            this.client.cache.messages.set(m.id, m);
+        });
+
+        return map;
     }
 }
