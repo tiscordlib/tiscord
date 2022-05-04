36b926bd89d12731d2c17c3d299808a5e6a412ec
xiboon <38988733+xiboon@users.noreply.github.com>
2022-05-04 22:02:44 +0200

    fix: formatting

--- packages/tiscord/src/events/reconnect.ts
+++ packages/tiscord/src/events/reconnect.ts
@@ -5,16 +5,14 @@ export async function reconnect(client: Client) {
     client.ws.connection.close();
     client.ws.connect();
     client.ws.connection.on('open', () => {
-        client.ws.send(
-            {
-                op: 6,
-                d: {
-                    token: client.token,
-                    session_id: client.ws.sessionId,
-                    seq: client.ws.sequence
-                }
+        client.ws.send({
+            op: 6,
+            d: {
+                token: client.token,
+                session_id: client.ws.sessionId,
+                seq: client.ws.sequence
             }
-        )
+        });
     });
     client.emit('reconnect');
 }
