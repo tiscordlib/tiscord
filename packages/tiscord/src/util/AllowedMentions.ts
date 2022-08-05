36849da225e5cd6690d47a5176598aaec0902b35
xiboon <38988733+xiboon@users.noreply.github.com>
2022-08-05 12:16:43 +0200

    docs: add documentation for allowedmentions

--- packages/tiscord/src/util/AllowedMentions.ts
+++ packages/tiscord/src/util/AllowedMentions.ts
@@ -1,5 +1,14 @@
 import { AllowedMentionsTypes } from 'discord-api-types/v10';
 
+/**
+ * A class for parsing allowed mentions to an API object.
+ * @param {RawMentions} mentions - Data to parse
+ * @class
+ * @property {AllowedMentionsTypes} [parse] - Mentions that should be parsed
+ * @property {string[]} [users] - The allowed users
+ * @property {string[]} [roles] - The allowed roles
+ * @property {string[]} [replied_user] - Whether to mention the user we're replying to
+ */
 export class AllowedMentions {
     parse?: AllowedMentionsTypes[];
     replied_user?: boolean;
@@ -12,6 +21,14 @@ export class AllowedMentions {
         if (mentions.repliedUser) this.replied_user = mentions.repliedUser;
     }
 }
+
+/**
+ * Data for allowed mentions to be later parsed.
+ * @property {AllowedMentionsTypes[]} parse - Mentions that should be parsed
+ * @property {string[]} roles - The allowed roles
+ * @property {string[]} users - The allowed users
+ * @property {boolean} repliedUser - Whether to mention the user we're replying to
+ */
 export interface RawMentions {
     parse?: AllowedMentionsTypes[];
     roles?: string[];
